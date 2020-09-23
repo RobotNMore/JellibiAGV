9955b99404ef677e59064e4c4ac8b323317b6905
RobotNMore <57121256+RobotNMore@users.noreply.github.com>
2020-09-23 14:45:57 +0900

    Update IOCheck.ino
    Servo Limit 모드 추가

--- src/00.UnitTest/IOCheck/IOCheck.ino
+++ src/00.UnitTest/IOCheck/IOCheck.ino
@@ -1,31 +1,31 @@
 
-/** 
- *  Copyright (c) 2019 Robot&More, jhlee@robotnmore.com(signaled@gmail.com)
- *  
- *  See the file license.txt for copying permission  
- */
+/**
+    Copyright (c) 2019 Robot&More, jhlee@robotnmore.com(signaled@gmail.com)
+
+    See the file license.txt for copying permission
+*/
 
 /**
- * JellibiAGV 보드용 샘플 
- *   D2 : RFID Reader control : SPI SS(Slave Select)
- *   D3 : Buzzer (Piezo Speaker)
- *   D4 : RFID Reader control : Module Reset
- *   D5 : Left side DC MOTOR : PWM  : 회전속도제어
- *   D6 : Right side DC MOTOR : PWM : 회전속도제어
- *   D7 : Left side DC MOTOR : GPIO : 회전방향제어
- *   D8 : Right side DC MOTOR : GPIO : 회전방향제어
- *   D9 : Servo 1  : PWM
- *   D10 : Servo 2 : PWM
- *   D11 : RFID Reader control : SPI MOSI 
- *   D12 : RFID Reader control : SPI MISO 
- *   D13 : RFID Reader control : SPI SCL
- *   A3 : 측면 사용자 버튼 : GPI
- *   A0 : 전면 센서보드 가운데 IR 센서 : ADC
- *   A1 : 전면 센서보드 왼쪽 IR 센서 : ADC
- *   A2 : 전면 센서보드 오른쪽 IR 센서 : ADC 
- *   A6 : 바닥면 센서보드 왼쪽 IR 센서 : ADC
- *   A7 : 바닥면 센서보드 오른쪽 IR 센서 : ADC
- */
+   JellibiAGV 보드용 샘플
+     D2 : RFID Reader control : SPI SS(Slave Select)
+     D3 : Buzzer (Piezo Speaker)
+     D4 : RFID Reader control : Module Reset
+     D5 : Left side DC MOTOR : PWM  : 회전속도제어
+     D6 : Right side DC MOTOR : PWM : 회전속도제어
+     D7 : Left side DC MOTOR : GPIO : 회전방향제어
+     D8 : Right side DC MOTOR : GPIO : 회전방향제어
+     D9 : Servo 1  : PWM
+     D10 : Servo 2 : PWM
+     D11 : RFID Reader control : SPI MOSI
+     D12 : RFID Reader control : SPI MISO
+     D13 : RFID Reader control : SPI SCL
+     A3 : 측면 사용자 버튼 : GPI
+     A0 : 전면 센서보드 가운데 IR 센서 : ADC
+     A1 : 전면 센서보드 왼쪽 IR 센서 : ADC
+     A2 : 전면 센서보드 오른쪽 IR 센서 : ADC
+     A6 : 바닥면 센서보드 왼쪽 IR 센서 : ADC
+     A7 : 바닥면 센서보드 오른쪽 IR 센서 : ADC
+*/
 #include <JellibiButton.h>
 
 // {{{ User Button
@@ -33,7 +33,7 @@ JellibiButton _button;
 #define USER_BUTTON A3
 // }}}
 
-// {{{ Drive Function 
+// {{{ Drive Function
 #define WHEEL_RIGHT_PWM   6
 #define WHEEL_RIGHT_DIR   8
 #define WHEEL_LEFT_PWM    5
@@ -43,7 +43,7 @@ void InitWheel();
 void TestDrive();
 void TestTurnRight();
 void TestTurnLeft();
-// }}} 
+// }}}
 
 // {{{ RFID Reader
 // Reference : https://randomnerdtutorials.com/security-access-using-mfrc522-rfid-reader-with-arduino/
@@ -61,7 +61,7 @@ void InitRFIDReader();
 bool ReadRFIDReader();
 // }}}
 
-// {{{ IR (A0,A1, A2, A6, A7) 
+// {{{ IR (A0,A1, A2, A6, A7)
 #define IR_FRONT_LEFT_PIN     A1
 #define IR_FRONT_CENTER_PIN   A0
 #define IR_FRONT_RIGHT_PIN    A2
@@ -70,13 +70,13 @@ bool ReadRFIDReader();
 void SensingIR();
 // }}}
 
-// {{{ Buzzer(D3) 
+// {{{ Buzzer(D3)
 #include "Tone_note.h"
 #define BUZZER_PIN 3 // PIEZO SPEAKER
 void PlayTone();
 // }}}
 
-// {{{ Servo (D9, D10) 
+// {{{ Servo (D9, D10)
 #include <Servo.h>
 
 #define SERVO1_PIN   9
@@ -88,6 +88,9 @@ void PlayTone();
 #define SERVO2_MIN  0   // MinDown
 #define SERVO2_MAX  90-10 // MaxUp-10
 #define SERVO2_DEF  SERVO2_MIN
+
+#define SERVO1_MAX_LIMIT  SERVO1_MAX-25
+#define SERVO2_MAX_LIMIT  SERVO2_MAX+25
 
 Servo _servo1;
 Servo _servo2;
@@ -110,7 +113,7 @@ void setup() {
 }
 
 void loop() {
-  
+
   // put your main code here, to run repeatedly:
   if (!_button.Check())
   {
@@ -122,18 +125,29 @@ void loop() {
   delay(1000);
   PutDown();
   delay(1000);
-  while(!ReadRFIDReader()){
+  while (!ReadRFIDReader()) {
     delay(500);
   }
   PlayTone();
   SensingIR();
+  int cnt = 0;
+  while (1) {
+    if (_button.Check())
+    {
+      cnt++;
+    }
+    if (cnt == 3) {
+      LiftUp_LIMIT();
+      break;
+    }
+  }
 }
 
 void PlayTone()
 {
   // notes in the melody:
   int melody[] = {  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4};
-  
+
   // note durations: 4 = quarter note, 8 = eighth note, etc.:
   int noteDurations[] = {4, 8, 8, 4, 4, 4, 4, 4};
   // iterate over the notes of the melody:
@@ -158,9 +172,10 @@ void InitServo()
 {
   _servo1.attach(SERVO1_PIN);
   _servo2.attach(SERVO2_PIN);
-  delay(100);
-  _servo1.write(SERVO1_DEF);
-  _servo2.write(SERVO2_DEF);
+  delay(300);
+  _servo1.write( SERVO1_MIN );
+  _servo2.write( SERVO2_MIN );
+  delay(300);
   _servo1.detach();
   _servo2.detach();
 }
@@ -172,12 +187,12 @@ void LiftUp()
   _servo2.attach(SERVO2_PIN);
   Serial.println(__FUNCTION__);
 
-  for ( int i =0; i <= STEP; i++) {
+  for ( int i = 0; i <= STEP; i++) {
     int angle1 = map(i, 0, STEP, SERVO1_MIN, SERVO1_MAX);
     int angle2 = map(i, 0, STEP, SERVO2_MIN, SERVO2_MAX);
     _servo1.write(angle1);
     _servo2.write(angle2);
-    delay(20);  
+    delay(20);
   }
 }
 
@@ -186,9 +201,9 @@ void PutDown()
   const int STEP = 20;
   Serial.println(__FUNCTION__);
 
-  for (int i=0; i<= STEP;i++) {
-    int angle1 = map(i,0, STEP, SERVO1_MAX, SERVO1_MIN);
-    int angle2 = map(i,0, STEP, SERVO2_MAX, SERVO2_MIN);
+  for (int i = 0; i <= STEP; i++) {
+    int angle1 = map(i, 0, STEP, SERVO1_MAX, SERVO1_MIN);
+    int angle2 = map(i, 0, STEP, SERVO2_MAX, SERVO2_MIN);
     _servo1.write(angle1);
     _servo2.write(angle2);
     delay(20);
@@ -196,6 +211,24 @@ void PutDown()
   _servo1.detach();
   _servo2.detach();
 }
+void LiftUp_LIMIT()
+{
+  const int STEP = 20;
+  _servo1.attach(SERVO1_PIN);
+  _servo2.attach(SERVO2_PIN);
+
+  for ( int i = 0; i <= STEP; i++) {
+    int angle1 = map(i, 0, STEP, SERVO1_MIN, SERVO1_MAX_LIMIT);
+    int angle2 = map(i, 0, STEP, SERVO2_MIN, SERVO2_MAX_LIMIT);
+    _servo1.write(angle1);
+    _servo2.write(angle2);
+    delay(20);
+  }
+  _servo1.detach();
+  _servo2.detach();
+}
+
+
 
 
 void SensingIR()
@@ -206,7 +239,7 @@ void SensingIR()
   int frontCenter = analogRead(IR_FRONT_CENTER_PIN);
   int bottomRight = analogRead(IR_BOTTOM_RIGHT_PIN);
   int bottomLeft = analogRead(IR_BOTTOM_LEFT_PIN);
-  sprintf(debugMessage, "F %04d-%04d-%04d / B %04d-%04d", frontLeft,frontCenter,  frontRight, bottomLeft,bottomRight); 
+  sprintf(debugMessage, "F %04d-%04d-%04d / B %04d-%04d", frontLeft, frontCenter,  frontRight, bottomLeft, bottomRight);
   Serial.println(debugMessage);
 }
 
@@ -230,8 +263,8 @@ bool ReadRFIDReader()
   Serial.println("detect");
   _strCardUID = "";
   for (byte i = 0; i < _mfrc522.uid.size; i++) {
-     _strCardUID.concat(String(_mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-     _strCardUID.concat(String(_mfrc522.uid.uidByte[i], HEX));
+    _strCardUID.concat(String(_mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    _strCardUID.concat(String(_mfrc522.uid.uidByte[i], HEX));
   }
   Serial.print("CARD UID : ");
   _strCardUID.toUpperCase();
@@ -240,7 +273,7 @@ bool ReadRFIDReader()
 }
 
 void InitWheel()
-{ 
+{
   pinMode(WHEEL_RIGHT_PWM, OUTPUT);
   pinMode(WHEEL_RIGHT_DIR, OUTPUT);
   pinMode(WHEEL_LEFT_PWM, OUTPUT);
@@ -268,7 +301,7 @@ void TestDrive()
   analogWrite(WHEEL_LEFT_PWM, 60);
   delay(1500);
   analogWrite(WHEEL_RIGHT_PWM, 0);
-  analogWrite(WHEEL_LEFT_PWM, 0);    
+  analogWrite(WHEEL_LEFT_PWM, 0);
 }
 
 void TestTurnRight()
